2f5ef6f0149c39bef405b0213576203bcf4bf12a
RosJes <isaksson.jessica@gmail.com>
2020-07-18 21:47:04 +0200

    added ajax test

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 function todaysWeather(city) {
   console.log("Today's weather in " + city + " is 29 degrees Celsius");
+
+  $("body").ready(function () {
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/photos",
+      type: "GET",
+      success: function (result) {
+        let ul = document.getElementById("list");
+
+        for (var i = 0; i < result.length; i++) {
+          let li = document.createElement("li");
+          let img = document.createElement("img");
+
+          li.innerHTML = result[i].title + "<br></br>";
+          img.src = result[i].thumbnailUrl;
+
+          li.appendChild(img);
+          ul.appendChild(li);
+        }
+      },
+    });
+  });
 }
-function foreCast(days) {}
