87d30c22aab60a1c7e42484265d6c0a97d5af278
RosJes <isaksson.jessica@gmail.com>
2020-07-19 01:20:27 +0200

    span added

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 function todaysWeather(city) {
   console.log("Today's weather in " + city + " is 29 degrees Celsius");
-
-  $("body").ready(function () {
+  $(document).ready(function () {
+    var api_url = "http://api.openweathermap.org/";
+    var key = "936f2e7c80c5a35d539529f46f2c798b";
     $.ajax({
-      url: "https://jsonplaceholder.typicode.com/photos",
+      url: api_url + "data/2.5/weather?q=" + city + "&appid=" + key,
       type: "GET",
+      dataType: "json",
       success: function (result) {
+        weatherSource = "";
         let ul = document.getElementById("list");
-
-        for (var i = 0; i < result.length; i++) {
-          let li = document.createElement("li");
-          let img = document.createElement("img");
-
-          li.innerHTML = result[i].title + "<br></br>";
-          img.src = result[i].thumbnailUrl;
-
-          li.appendChild(img);
-          ul.appendChild(li);
+        let text = document.createTextNode(result.weather[0].description);
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        if (result.weather[0].description.includes("rain")) {
+          weatherSource =
+            "https://img.icons8.com/ios/50/000000/torrential-rain.png";
         }
+        if (result.weather[0].description == "light rain") {
+          weatherSource = "https://img.icons8.com/ios/50/000000/light-rain.png";
+        }
+        if (result.weather[0].description == "shower rain") {
+          weatherSource =
+            "https://img.icons8.com/ios/50/000000/intense-rain.png";
+        }
+        if (result.weather[0].description == "thunderstorm") {
+          weatherSource = "https://img.icons8.com/ios/50/000000/storm.png";
+        }
+        if (result.weather[0].description == "clear sky") {
+          weatherSource = "https://img.icons8.com/ios/50/000000/sun.png";
+        }
+        if (result.weather[0].description == "partly clouds") {
+          weatherSource =
+            "https://img.icons8.com/ios/50/000000/partly-cloudy-day.png";
+        }
+
+        if (result.weather[0].description.includes("snow")) {
+          weatherSource = "https://img.icons8.com/ios/50/000000/snow.png";
+        }
+        if (result.weather[0].description.includes("cloud")) {
+          weatherSource = "https://img.icons8.com/ios/50/000000/clouds.png";
+        }
+        img.src = weatherSource;
+        li.appendChild(text);
+        ul.appendChild(img);
+        ul.appendChild(li);
+        console.log(result.weather[0].description);
       },
     });
   });
